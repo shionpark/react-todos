a745589f2bb8ecd3f841fdfdc8078e07f4adcda8
bkkom <seoy1108@gmail.com>
2023-09-26 23:05:08 +0900

    Config: react-hooks 의존성 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function Home() {
-  const [toDo, setToDo] = useState('');
-  const [toDoError, setToDoError] = useState('');
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDoError('');
-    setToDo(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (toDo.length < 10) {
-      return setToDoError('To do should be longer');
-    }
-    console.log('submit');
-  };
-
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <h1>ToDos</h1>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <h1>ToDos</h1>
+      <form style={{ display: 'flex', flexDirection: 'column' }}>
+        <input {...register('email')} placeholder="Email" />
+        <input {...register('firstName')} placeholder="First Name" />
+        <input {...register('lastName')} placeholder="Last Name" />
+        <input {...register('username')} placeholder="Username" />
+        <input {...register('password')} placeholder="Password" />
+        <input {...register('password1')} placeholder="Password1" />
         <button>Add</button>
-        {toDoError !== '' ? toDoError : null}
       </form>
     </div>
   );

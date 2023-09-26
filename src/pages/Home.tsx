75e8a35078ac31fa835ffd53864066c7b97593a2
bkkom <seoy1108@gmail.com>
2023-09-26 23:03:36 +0900

    Chore: toDos form 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => {
-  return <h1>Home</h1>;
-};
+function Home() {
+  const [toDo, setToDo] = useState('');
+  const [toDoError, setToDoError] = useState('');
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDoError('');
+    setToDo(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError('To do should be longer');
+    }
+    console.log('submit');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <h1>ToDos</h1>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+        {toDoError !== '' ? toDoError : null}
+      </form>
+    </div>
+  );
+}
 
 export default Home;

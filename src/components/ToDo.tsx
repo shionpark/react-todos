5cd2e608b53ef209f6b1f4bcbd60aaa6b53ac63a
bkkom <seoy1108@gmail.com>
2023-09-27 00:39:00 +0900

    Refactor: atom 컴포넌트 분리

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IToDo } from '@/atoms';
+
+const ToDo = ({ text }: IToDo) => {
+  return (
+    <li>
+      <span>{text}</span>
+      <button>Doing</button>
+      <button>To Do</button>
+      <button>Done</button>
+    </li>
+  );
+};
+
+export default ToDo;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
+export { default as ToDo } from './ToDo';
 export { default as ToDoList } from './ToDoList';
+export { default as CreateDoTo } from './CreateDoTo';
